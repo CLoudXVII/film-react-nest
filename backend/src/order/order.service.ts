db88718af1b0de6fc649434b8d4aabfb178bd869
CLoudXVII <sharkov.vs@outlook.com>
2025-10-24 14:25:51 +0300

    feat: initial modules and setup

---
+++ backend/src/order/order.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Injectable()
+export class OrderService {
+  create(createOrderDto: CreateOrderDto) {
+    return 'This action creates a new order';
+  }
+}
